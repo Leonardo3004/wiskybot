20651a78f18128932162325dced41b93740a2b3a
Leonardo3004 <62188559+Leonardo3004@users.noreply.github.com>
2023-11-16 15:25:29 -0600

    aa

--- 01 - Setting up project/src/analisis.py
+++ 01 - Setting up project/src/analisis.py
@@ -19,13 +19,15 @@ cantidad_registros = 0
 #Iterar a través de las fechas y registros de emociones
 print(data)
 for registros in data.values():#itera las fechas
+    
     for registro in registros:#itera por cada emocion de la fecha
-        suma_angry += registro.get("angry", 0)
-        suma_happy += registro.get("happy", 0)
-        suma_neutral += registro.get("neutral", 0)
-        suma_disgust += registro.get("disgust", 0)
-        suma_fear += registro.get("fear", 0)
-        cantidad_registros += 1
+        if(len(registro) >1 ):
+            suma_angry += registro.get("angry", 0)
+            suma_happy += registro.get("happy", 0)
+            suma_neutral += registro.get("neutral", 0)
+            suma_disgust += registro.get("disgust", 0)
+            suma_fear += registro.get("fear", 0)
+            cantidad_registros += 1
 
 if cantidad_registros > 0:
     promedio_angry = suma_angry / cantidad_registros
@@ -33,6 +35,12 @@ if cantidad_registros > 0:
     promedio_neutral = suma_neutral / cantidad_registros
     promedio_disgust = suma_disgust / cantidad_registros
     promedio_fear = suma_fear / cantidad_registros
+    masGrande=max(promedio_angry,promedio_happy, promedio_neutral, promedio_disgust, promedio_fear)
+    mi_diccionario = {'Enojado': promedio_angry, 'Contento': promedio_happy, 'Neutral': promedio_neutral, 'Disconforme': promedio_disgust, 'Miedo':promedio_fear}
+    
+    clave_maxima = max(mi_diccionario, key=lambda k: mi_diccionario[k])
+    print("La clave con el valor máximo es:", clave_maxima)
+
     try:
         with open('example.txt', 'w') as file:
             file.write(f"El promedio de estudiantes 'enojados' es de': {round(promedio_angry, 3)}%.\n")
@@ -40,6 +48,7 @@ if cantidad_registros > 0:
             file.write(f"El promedio de estudiantes 'neutrales' es de: {round(promedio_neutral,3)}%.\n")
             file.write(f"El promedio de estudiantes 'disconformes' es de: {round(promedio_disgust,3)}%.\n")
             file.write(f"El promedio de estudiantes 'con miedo' es de: {round(promedio_fear,3)}%.\n")
+            file.write(f"La emocion mas predominante es '{clave_maxima}': {round(masGrande, 3)}%.\n")
 
     except IOError as e:
         print(f"An error occurred: {e}")
